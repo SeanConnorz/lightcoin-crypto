2bfd5a351c4b65a3509aa63e3893d34ab5bf3be1
Connor Hughes <seanandhughes@gmail.com>
2021-11-05 20:07:48 +0000

    Completed lightCoin

--- index.js
+++ index.js
@@ -1,13 +1,57 @@
-let balance = 500.00;
+class Account {
 
-class Withdrawal {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
 
-  constructor(amount) {
+  get balance() {
+    let balance = 0;
+    this.transactions.forEach((item) => {
+      balance += item;
+    })
+    return balance;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+
+}
+
+class Transaction {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    if (!this.isAlloud()) return false;
+    this.time = new Date();
+    this.account.addTransaction(this.value);
+  }
+}
+
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount;
+  }
+
+  isAlloud() {
+    return true;
+  }
+
+}
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount;
+  }
+
+  isAlloud() {
+    return (this.account.balance - this.amount >= 0)
   }
 
 }
@@ -15,15 +59,27 @@ class Withdrawal {
 
 
 
+
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account('snow-patrol');
+
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
+t1.balance;
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+t3 = new Deposit(120.00, myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);
+
+t3 = new Withdrawal(60, myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);
+
+console.log(myAccount.balance);
